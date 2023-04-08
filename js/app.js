0359377a85a696a0b74c1b3a0ee621d2214d9d9c
Mariusz <smajdortech@gmail.com>
2023-04-08 22:06:18 +0200

    Calculate currency exchange result

--- js/app.js
+++ js/app.js
@@ -1,13 +1,26 @@
 import { fetchCurrencies } from './fetchCurrencies.js';
 
 async function init() {
-  const convertFromEl = document.querySelector('.js-convert-form');
+  const formEl = document.querySelector('.js-form');
+  const resultEl = document.querySelector('.js-result');
+  const amountEl = document.querySelector('.js-amount');
+  const convertFromEl = document.querySelector('.js-convert-from');
   const convertToEl = document.querySelector('.js-convert-to');
 
   const currencies = await fetchCurrencies();
   Object.entries(currencies).forEach(curr => {
     convertFromEl.appendChild(createOptionEl(curr));
     convertToEl.appendChild(createOptionEl(curr));
+  });
+
+  formEl.addEventListener('submit', e => {
+    e.preventDefault();
+
+    resultEl.innerText = `${amountEl.value} ${
+      convertFromEl.options[convertFromEl.selectedIndex].text
+    } = ${((amountEl.value / convertFromEl.value) * convertToEl.value).toFixed(
+      2
+    )} ${convertToEl.options[convertToEl.selectedIndex].text}`;
   });
 }
 
