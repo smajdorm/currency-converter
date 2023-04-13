740287ef44a524a4d872cc9eb22c16f23001a399
Mariusz <smajdortech@gmail.com>
2023-04-13 17:07:05 +0200

    Split code into smaller more descriptive functions

--- js/app.js
+++ js/app.js
@@ -1,30 +1,32 @@
 import { fetchCurrencies } from './fetchCurrencies.js';
 
-async function init() {
-  const formEl = document.querySelector('.js-form');
+const formEl = document.querySelector('.js-form');
+const convertFromEl = document.querySelector('.js-convert-from');
+const convertToEl = document.querySelector('.js-convert-to');
+
+const currencies = await fetchCurrencies();
+Object.entries(currencies).forEach(curr => {
+  convertFromEl.appendChild(createOptionEl(curr));
+  convertToEl.appendChild(createOptionEl(curr));
+});
+
+formEl.addEventListener('submit', e => {
+  e.preventDefault();
+  createResultElement();
+});
+
+function createResultElement() {
   const resultEl = document.querySelector('.js-result');
   const amountEl = document.querySelector('.js-amount');
-  const convertFromEl = document.querySelector('.js-convert-from');
-  const convertToEl = document.querySelector('.js-convert-to');
 
-  const currencies = await fetchCurrencies();
-  Object.entries(currencies).forEach(curr => {
-    convertFromEl.appendChild(createOptionEl(curr));
-    convertToEl.appendChild(createOptionEl(curr));
-  });
-
-  formEl.addEventListener('submit', e => {
-    e.preventDefault();
-
-    resultEl.innerHTML = `${amountEl.value} <strong>${
-      convertFromEl.options[convertFromEl.selectedIndex].text
-    }</strong> = ${(
-      (amountEl.value / convertFromEl.value) *
-      convertToEl.value
-    ).toFixed(2)} <strong>${
-      convertToEl.options[convertToEl.selectedIndex].text
-    }</strong>`;
-  });
+  resultEl.innerHTML = `${amountEl.value} <strong>${
+    convertFromEl.options[convertFromEl.selectedIndex].text
+  }</strong> = ${(
+    (amountEl.value / convertFromEl.value) *
+    convertToEl.value
+  ).toFixed(2)} <strong>${
+    convertToEl.options[convertToEl.selectedIndex].text
+  }</strong>`;
 }
 
 function createOptionEl(curr) {
@@ -34,5 +36,3 @@ function createOptionEl(curr) {
 
   return optionEl;
 }
-
-init();
